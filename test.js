0a5abbc26d9cb84321f026af388c2928ffaf3825
Azeem <jazeem.lk@gmail.com>
2017-03-02 11:46:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 
+var reg = [{
+“id”: “123”,
+“value”: “Test first text to speech”,
+}, {
+“id”: “1234”,
+“value”: “Second text to speech”,
+}];
+
+PollyImplemetation(req, function() {
+
+ 
+
+});
+
